c49b206945909468cbf4f489364ebd97716849a1
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-02-27 21:16:03 +0530

    Update __main__.py

--- ItachiRobot/__main__.py
+++ ItachiRobot/__main__.py
@@ -266,7 +266,7 @@ def help_button(update, context):
     try:
         if mod_match:
             module = mod_match.group(1)
-            text = ("──「 Help for the *{}* module:\n 」──".format(
+            text = ("Help for the *{}* module:\n".format(
                 HELPABLE[module].__mod_name__) + HELPABLE[module].__help__)
             query.message.edit_text(
                 text=text,
