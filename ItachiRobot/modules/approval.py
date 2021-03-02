91e8f535345b351ee6ee5fe88e197023cabfef82
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-03-02 16:20:23 +0530

    Update approval.py

--- ItachiRobot/modules/approval.py
+++ ItachiRobot/modules/approval.py
@@ -1,11 +1,11 @@
 import html
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot import dispatcher, DRAGONS
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
+from ItachiRobot.modules.disable import DisableAbleCommandHandler
+from ItachiRobot import dispatcher, DRAGONS
+from ItachiRobot.modules.helper_funcs.extraction import extract_user
 from telegram.ext import CallbackContext, run_async, CallbackQueryHandler
-import SaitamaRobot.modules.sql.approve_sql as sql
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.log_channel import loggable
+import ItachiRobot.modules.sql.approve_sql as sql
+from ItachiRobot.modules.helper_funcs.chat_status import user_admin
+from ItachiRobot.modules.log_channel import loggable
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.utils.helpers import mention_html
 from telegram.error import BadRequest
